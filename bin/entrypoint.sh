999b690c8758a9a6a9245ee43a80d65d3399b14c
Javi Corbin <javi.corbin@gmail.com>
2021-04-11 16:16:57 +0200

    Added bash as an option

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-mkdir /output && cd /output || exit
-wine /app/eac3to/eac3to.exe z:/raws "1)" -demux -progressnumbers
+mkdir -p /output && cd /output || exit
+
+if [ -n "$1" ]; then
+    set -x
+    exec "$@"
+else
+  wine /app/eac3to/eac3to.exe z:/raws "1)" -demux -progressnumbers
+fi
